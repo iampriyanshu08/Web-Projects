d82e08c271b86d84cc9c2f527404a6a435ab1898
Priyanshu Utsahi <utsahiji.priyanshu2000@gmail.com>
2024-02-05 19:56:11 +0530

    added smooth scroll

--- Project-1/script.js
+++ Project-1/script.js
@@ -2,7 +2,7 @@ const mouseCursor = document.querySelector("#cursor");
 const cursorBlur = document.querySelector("#cursor-blur");
 
 document.addEventListener("mousemove", function (e) {
-  mouseCursor.style.left = e.clientX  + "px";
+  mouseCursor.style.left = e.clientX + "px";
   mouseCursor.style.top = e.clientY + "px";
   cursorBlur.style.left = e.clientX - 250 + "px";
   cursorBlur.style.top = e.clientY - 250 + "px";
@@ -24,6 +24,19 @@ h4All.forEach(function (elem) {
     mouseCursor.style.backgroundColor = "red";
   });
 });
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
 
 gsap.to("#nav", {
   backgroundColor: "red",
@@ -50,18 +63,72 @@ gsap.to("#main", {
   },
 });
 
-
-gsap.from( "#about-us img, #about-us-in" , {
-  y:50,
-  opacity : 0,
-  duration : 1,
-  stagger  :0.4,
-  scrollTrigger : {
+gsap.from("#about-us img, #about-us-in", {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.4,
+  scrollTrigger: {
     trigger: "#about-us",
     scroller: "body",
-    markers: true,
-    start: "top 60%",
-    end: "top 58%",
+    // markers: true,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+gsap.from(".card", {
+  scale: 0.8,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.4,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    // markers: true,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+
+gsap.from("#colon1", {
+  y: -70,
+  x: -70,
+
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers : true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#colon2", {
+  y: 70,
+  x: 70,
+
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers : true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+
+gsap.from("#page4 h1", {
+  y: 50,
+  scrollTrigger: {
+    trigger: "#page4 h1",
+    scroller: "body",
+    // markers : true,
+    start: "top 75%",
+    end: "top 70%",
     scrub: 3,
-  }
-})
+  },
+});
+
+// create the scrollSmoother before your scrollTriggers
